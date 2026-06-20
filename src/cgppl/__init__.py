65db519fba3b60d084c3b1c4f01371be230df937
Ömer Faruk Nehir <92220159+omerfaruknehir@users.noreply.github.com>
2026-06-21 01:59:36 +0300

    Export semantic validation API

--- src/cgppl/__init__.py
+++ src/cgppl/__init__.py
@@ -2,3 +2,4 @@
 
 from .lexer import Lexer, LexerError, Token, TokenKind, tokenize
 from .parser import Parser, ParserError, parse_program
+from .semantics import Diagnostic, SemanticError, validate_program
