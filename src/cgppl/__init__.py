602b67559c6ec8b7f37b4aa46be66f1f402e9eba
Ömer Faruk Nehir <92220159+omerfaruknehir@users.noreply.github.com>
2026-06-21 14:58:55 +0300

    Export attribute predicate AST helper

--- src/cgppl/__init__.py
+++ src/cgppl/__init__.py
@@ -1,6 +1,6 @@
 """CGPPL language implementation package."""
 
-from .ast import TryOrStmt, VarRef
+from .ast import AttrPredicate, TryOrStmt, VarRef
 from .graph import Edge, Graph, GraphError, Node
 from .lexer import Lexer, LexerError, Token, TokenKind, tokenize
 from .parser import Parser, ParserError, parse_program
